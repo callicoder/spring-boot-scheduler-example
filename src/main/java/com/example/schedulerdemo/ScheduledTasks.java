00e128e17de14dcb6e92ab74d41b224a333232ca
Rajeev Kumar Singh <rajeevs@flock.co>
2017-12-27 15:36:55 +0530

    using DateTimeFormatter to format date

--- src/main/java/com/example/schedulerdemo/ScheduledTasks.java
+++ src/main/java/com/example/schedulerdemo/ScheduledTasks.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,16 +17,16 @@ public class ScheduledTasks {
 
     private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     @Scheduled(fixedRate = 2000)
     public void scheduleTaskWithFixedRate() {
-        logger.info("Fixed Rate Task :: Execution Time - {}", dateFormat.format(new Date()) );
+        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
     }
 
     @Scheduled(fixedDelay = 2000)
     public void scheduleTaskWithFixedDelay() {
-        logger.info("Fixed Delay Task :: Execution Time - {}", dateFormat.format(new Date()));
+        logger.info("Fixed Delay Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
         try {
             TimeUnit.SECONDS.sleep(5);
         } catch (InterruptedException ex) {
@@ -37,12 +37,12 @@ public class ScheduledTasks {
 
     @Scheduled(fixedRate = 2000, initialDelay = 5000)
     public void scheduleTaskWithInitialDelay() {
-        logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateFormat.format(new Date()));
+        logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
     }
 
     @Scheduled(cron = "0 * * * * ?")
     public void scheduleTaskWithCronExpression() {
-        logger.info("Cron Task :: Execution Time - {}", dateFormat.format(new Date()));
+        logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
     }
 
 }
